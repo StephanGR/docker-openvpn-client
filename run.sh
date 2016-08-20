18955ac95350f952954b906cd3a587354d97dce7
Clément <213737@supinfo.com>
2016-08-20 21:08:00 +0200

    endpoint for dockerfile

---
+++ run.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+ENDL=%0A
+sleep 5
+echo $OPENVPN_USERNAME > openvpn-credentials.txt
+echo $OPENVPN_PASSWORD >> openvpn-credentials.txt
+chmod 600 openvpn-credentials.txt
+cp /usr/share/zoneinfo/Europe/Luxembourg /etc/localtime
+while true; do
+  a=(*);
+  FILE_CONFIG=${PWD}/${a[$((RANDOM % ${#a[@]}))]}
+  echo $FILE_CONFIG
+  openvpn $OPENVPN_OPTS --config "$FILE_CONFIG" &
+  sleep 45
+  IP=`curl ipecho.net/plain`
+  HOSTNAME=`curl ipinfo.io/$IP | jq '.hostname'`
+  CITY=`curl ipinfo.io/$IP | jq '.city'`
+  COUNTRY=`curl ipinfo.io/$IP | jq '.country'`
+  ORG=`curl ipinfo.io/$IP | jq '.org'`
+  INFO=`python3 /bin/speedtest-cli --simple`
+  SEPARATOR="-----------------------------------------"
+  msg="Hello, new VPN (`basename $FILE_CONFIG`)  at `date +"%T %a %d %h %y"` ${ENDL}${ENDL} $HOSTNAME $IP in ${ENDL} CITY : $CITY ${ENDL} COUNTRY : $COUNTRY ${ENDL} ORGANISATION :  $ORG ${ENDL}$SEPARATOR${ENDL} $INFO"
+  ./bot.sh --token=$TOKEN --id=$USERID --text="$msg"
+  sleep $TIMESWITCH
+  pkill openvpn
+done
